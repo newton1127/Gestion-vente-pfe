6597628a2f25eff4325e0334b73075b23684614f
newton1127 <maanaoui1127@gmail.com>
2023-01-08 12:15:30 +0100

    second commit

--- src/main/java/com/sid/clientservice/repos/ClientRepos.java
+++ src/main/java/com/sid/clientservice/repos/ClientRepos.java
@@ -2,7 +2,9 @@ package com.sid.clientservice.repos;
 
 import com.sid.clientservice.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ClientRepos extends JpaRepository<Client,Long> {
     public Client findClientByNom(String nom);
     public Client findClientById(Long id);
